e69e361f955277fcb14a84ecd3860e36e86b2333
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-22 11:54:44 -0400

    cuenta regresiva actualizada, con fecha de inicio y fecha final

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,112 +1,25 @@
-let reloj = localStorage.getItem("contador")
-  ? JSON.parse(localStorage.getItem("contador"))
-  : {
-      day: 8,
-      hours: 4,
-      minutes: 1,
-      seconds: 4,
-    };
+let fechaFinal = new Date("2023-06-30T00:00:00"); // Fecha y hora final
 
-//Definimos y ejecutamos los segundos
-function cargarSegundos() {
-  let txtSeconds;
+function actualizarContador() {
+  let ahora = new Date();
+  let diferencia = fechaFinal - ahora; // Diferencia en milisegundos
 
-  if (reloj.seconds < 0) {
-    reloj.seconds = 59;
+  // Si la diferencia es negativa, el tiempo ha pasado
+  if (diferencia < 0) {
+    diferencia = 0;
   }
 
-  //Mostrar seconds en pantalla
-  if (reloj.seconds < 10) {
-    txtSeconds = "0" + reloj.seconds;
-  } else {
-    txtSeconds = reloj.seconds;
-  }
-  document.getElementById("seconds").innerHTML = txtSeconds;
-  reloj.seconds--;
+  let segundos = Math.floor(diferencia / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
 
-  cargarMinutos(reloj.seconds);
-
-  // Guardar el estado actual en localStorage
-  localStorage.setItem("contador", JSON.stringify(reloj));
+  // Actualizar los elementos de la página con los nuevos valores
+  document.getElementById("seconds").innerHTML = segundos % 60;
+  document.getElementById("minutes").innerHTML = minutos % 60;
+  document.getElementById("hours").innerHTML = horas % 24;
+  document.getElementById("days").innerHTML = dias;
 }
 
-//Definimos y ejecutamos los minutos
-function cargarMinutos(seconds) {
-  let txtMinutes;
-
-  if (seconds == -1 && reloj.minutes !== 0) {
-    setTimeout(() => {
-      reloj.minutes--;
-    }, 500);
-  } else if (seconds == -1 && reloj.minutes == 0) {
-    setTimeout(() => {
-      reloj.minutes = 59;
-    }, 500);
-  }
-
-  //Mostrar minutes en pantalla
-  if (reloj.minutes < 10) {
-    txtMinutes = "0" + reloj.minutes;
-  } else {
-    txtMinutes = reloj.minutes;
-  }
-  document.getElementById("minutes").innerHTML = txtMinutes;
-  cargarHoras(reloj.seconds, reloj.minutes);
-
-  // Guardar el estado actual en localStorage
-  localStorage.setItem("contador", JSON.stringify(reloj));
-}
-
-function cargarHoras(seconds, minutes) {
-  let txtHours;
-
-  if (seconds == -1 && minutes == 0 && reloj.hours !== 0) {
-    setTimeout(() => {
-      reloj.hours--;
-    }, 500);
-  } else if (seconds == -1 && minutes == 0 && reloj.hours !== 0) {
-    setTimeout(() => {
-      reloj.hours = 2;
-    }, 500);
-  }
-
-  //Mostrar hours en pantalla
-  if (reloj.hours < 10) {
-    txtHours = "0" + reloj.hours;
-  } else {
-    txtHours = reloj.hours;
-  }
-  document.getElementById("hours").innerHTML = txtHours;
-  cargarDias(reloj.seconds, reloj.minutes, reloj.hours);
-
-  // Guardar el estado actual en localStorage
-  localStorage.setItem("contador", JSON.stringify(reloj));
-}
-
-function cargarDias(seconds, minutes, hours) {
-  let txtDays;
-
-  if (seconds == -1 && minutes == 0 && hours == 0 && reloj.day == 0) {
-    setTimeout(() => {
-      reloj.day--;
-    }, 500);
-  } else if (seconds == -1 && minutes == 0 && hours == 0 && reloj.day == 0) {
-    setTimeout(() => {
-      reloj.day = 30;
-    }, 500);
-  }
-
-  // Mostrar días en pantalla
-  if (reloj.day < 10) {
-    txtDays = "0" + reloj.day;
-  } else {
-    txtDays = reloj.day;
-  }
-  document.getElementById("days").innerHTML = txtDays;
-
-  // Guardar el estado actual en localStorage
-  localStorage.setItem("contador", JSON.stringify(reloj));
-}
-
-//Ejecutamos cada second
-setInterval(cargarSegundos, 1000);
+// Actualizar el contador cada segundo
+setInterval(actualizarContador, 1000);
