d3d62d084c25e473ed93ecc208665d1816452101
Miguel Teran <miguel.teranj02@gmail.com>
2023-06-22 10:27:29 -0400

    countdown terminado para el 30 de junio

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,23 +1,112 @@
-const w = window;
-const d = document;
+let reloj = localStorage.getItem("contador")
+  ? JSON.parse(localStorage.getItem("contador"))
+  : {
+      day: 8,
+      hours: 4,
+      minutes: 1,
+      seconds: 4,
+    };
 
-w.addEventListener("DOMContentLoaded", () => {
-  let totalSegundos = 9 * 24 * 60 * 60;
+//Definimos y ejecutamos los segundos
+function cargarSegundos() {
+  let txtSeconds;
 
-  const intervalID = setInterval(() => {
-    totalSegundos--;
+  if (reloj.seconds < 0) {
+    reloj.seconds = 59;
+  }
 
-    const dias = Math.floor(totalSegundos / (24 * 60 * 60));
-    const horas = Math.floor((totalSegundos % (24 * 60 * 60)) / (60 * 60));
-    const minutos = Math.floor(totalSegundos % (60 * 60)) / 60;
-    const segundos = Math.floor(totalSegundos % 60);
+  //Mostrar seconds en pantalla
+  if (reloj.seconds < 10) {
+    txtSeconds = "0" + reloj.seconds;
+  } else {
+    txtSeconds = reloj.seconds;
+  }
+  document.getElementById("seconds").innerHTML = txtSeconds;
+  reloj.seconds--;
 
-    d.getElementById(
-      "countdown"
-    ).innerText = `${dias} ${horas} ${minutos} ${segundos}`;
+  cargarMinutos(reloj.seconds);
 
-    if (totalSegundos === 0) {
-      clearInterval(intervalID);
-    }
-  }, 1000);
-});
+  // Guardar el estado actual en localStorage
+  localStorage.setItem("contador", JSON.stringify(reloj));
+}
+
+//Definimos y ejecutamos los minutos
+function cargarMinutos(seconds) {
+  let txtMinutes;
+
+  if (seconds == -1 && reloj.minutes !== 0) {
+    setTimeout(() => {
+      reloj.minutes--;
+    }, 500);
+  } else if (seconds == -1 && reloj.minutes == 0) {
+    setTimeout(() => {
+      reloj.minutes = 59;
+    }, 500);
+  }
+
+  //Mostrar minutes en pantalla
+  if (reloj.minutes < 10) {
+    txtMinutes = "0" + reloj.minutes;
+  } else {
+    txtMinutes = reloj.minutes;
+  }
+  document.getElementById("minutes").innerHTML = txtMinutes;
+  cargarHoras(reloj.seconds, reloj.minutes);
+
+  // Guardar el estado actual en localStorage
+  localStorage.setItem("contador", JSON.stringify(reloj));
+}
+
+function cargarHoras(seconds, minutes) {
+  let txtHours;
+
+  if (seconds == -1 && minutes == 0 && reloj.hours !== 0) {
+    setTimeout(() => {
+      reloj.hours--;
+    }, 500);
+  } else if (seconds == -1 && minutes == 0 && reloj.hours !== 0) {
+    setTimeout(() => {
+      reloj.hours = 2;
+    }, 500);
+  }
+
+  //Mostrar hours en pantalla
+  if (reloj.hours < 10) {
+    txtHours = "0" + reloj.hours;
+  } else {
+    txtHours = reloj.hours;
+  }
+  document.getElementById("hours").innerHTML = txtHours;
+  cargarDias(reloj.seconds, reloj.minutes, reloj.hours);
+
+  // Guardar el estado actual en localStorage
+  localStorage.setItem("contador", JSON.stringify(reloj));
+}
+
+function cargarDias(seconds, minutes, hours) {
+  let txtDays;
+
+  if (seconds == -1 && minutes == 0 && hours == 0 && reloj.day == 0) {
+    setTimeout(() => {
+      reloj.day--;
+    }, 500);
+  } else if (seconds == -1 && minutes == 0 && hours == 0 && reloj.day == 0) {
+    setTimeout(() => {
+      reloj.day = 30;
+    }, 500);
+  }
+
+  // Mostrar días en pantalla
+  if (reloj.day < 10) {
+    txtDays = "0" + reloj.day;
+  } else {
+    txtDays = reloj.day;
+  }
+  document.getElementById("days").innerHTML = txtDays;
+
+  // Guardar el estado actual en localStorage
+  localStorage.setItem("contador", JSON.stringify(reloj));
+}
+
+//Ejecutamos cada second
+setInterval(cargarSegundos, 1000);
